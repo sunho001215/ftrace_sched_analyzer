81d9ebef6386bba71f759145540ebd1c79ddec33
HayeonP <kite9240@gmail.com>
2022-07-26 00:36:40 +0900

    per_thread and per_cpu mode is added to log_viz_df.py

--- scripts/log_viz_df.py
+++ scripts/log_viz_df.py
@@ -8,6 +8,11 @@ from tqdm import tqdm
 from plotly.subplots import make_subplots
 import pandas as pd
 
+############### TODO ###############
+# visualization mode ("per_thread" or "per_cpu")
+mode = 'per_thread'
+####################################
+
 def load_data(data_path):
     with open(data_path) as f:
         raw_data = json.load(f)
@@ -21,6 +26,7 @@ def load_data(data_path):
                 df = pd.json_normalize(sched_data[name])
                 if 'Start Time' not in df: continue
                 df['Name'] = str(name)
+                df['Core'] = str(core)
                 df['Duration'] = df['End Time'] - df['Start Time']
                 df['Label'] = df['Name'] + '(' + df['PID'].astype(str) + ')'
                 df['StartTime'] = df['Start Time']
@@ -31,7 +37,7 @@ def load_data(data_path):
 
     return sched_info_dfs
 
-def visualize_all_cores(sched_info_dfs):
+def visualize_all_cores_per_thread(sched_info_dfs):
     config = dict({'scrollZoom': True})
 
     fig = go.Figure(
@@ -51,7 +57,7 @@ def visualize_all_cores(sched_info_dfs):
                 name=pid)
 
         fig.update_layout(
-            title=core+"Analysis Result",
+            title=core+" scheduling",
             xaxis_title="time (s)",
             barmode='stack',
             font=dict(
@@ -60,14 +66,48 @@ def visualize_all_cores(sched_info_dfs):
                 color="#7f7f7f"
             )
         )
+
         fig.show()
     
     
     return
 
+def visualize_all_cores_per_cpu(sched_info_dfs):
+    config = dict({'scrollZoom': True})
+
+    fig = go.Figure(
+    layout = {
+        'barmode': 'stack',
+        'xaxis': {'automargin': True},
+        'yaxis': {'automargin': True}}#, 'categoryorder': 'category ascending'}}
+    )
+
+    for core in sched_info_dfs:
+        df = sched_info_dfs[core]
+        fig.add_bar(x=df.Duration,
+                    y=df.Core,
+                    base=df.StartTime,
+                    orientation='h',
+                    showlegend=False,
+                    text=df.Label
+                    )
+
+        fig.update_layout(
+            title="All scheduling",
+            xaxis_title="time (s)",
+            barmode='stack',
+            font=dict(
+                family="Courier New, monospace",
+                color="#7f7f7f"
+            )
+        )
+    fig.show()
 
 if __name__ == '__main__':
     file_path = os.path.dirname(os.path.realpath(__file__))[0:-7] + "/data/sample.json"
     # file_path = os.path.dirname(os.path.realpath(__file__))[0:-7] + "/data/ftrace_parse_data.json"
     sched_info_dfs = load_data(file_path)
-    visualize_all_cores(sched_info_dfs)
+    if mode == 'per_thread':
+        visualize_all_cores_per_thread(sched_info_dfs)
+    elif mode == 'per_cpu':
+        visualize_all_cores_per_cpu(sched_info_dfs)
